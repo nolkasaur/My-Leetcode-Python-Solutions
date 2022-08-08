6b00311bdc344b2275238ecc58008f11361c7f4d
Bruno Firmino <brunoftlk@gmail.com>
2022-08-08 21:41:57 +0100

    Update 1108.py

--- Problem Listing/1108.py
+++ Problem Listing/1108.py
@@ -1,5 +1,5 @@
-// https://leetcode.com/problems/defanging-an-ip-address/
-// 41 ms, 13.7 MB
+# https://leetcode.com/problems/defanging-an-ip-address/
+# 41 ms, 13.7 MB
 
 class Solution:
     def defangIPaddr(self, address: str) -> str:
