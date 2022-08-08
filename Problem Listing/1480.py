11d7848a71ad0e9e16d3b2aefa2972486d3c9435
Bruno Firmino <brunoftlk@gmail.com>
2022-08-08 21:12:14 +0100

    Create 1480.py

---
+++ Problem Listing/1480.py
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/running-sum-of-1d-array/
+// 57 ms, 14 MB
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for x in range(1, len(nums)):
+            nums[x] = nums[x]+nums[x-1]
+        return nums
