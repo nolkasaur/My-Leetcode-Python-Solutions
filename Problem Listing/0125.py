d411e13b72cb39ce2e906a48b51f8137e4818327
Bruno Firmino <brunoftlk@gmail.com>
2024-09-20 19:19:14 +0100

    Create 0125.py

---
+++ Problem Listing/0125.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/valid-palindrome/
+# 52 ms, 21.81 MB
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #s = "".join(s.split())
+        s = "".join(c.lower() for c in s if c.isalnum())
+        for i in range(int(len(s)/2)):
+            if s[i] != s[len(s)-1-i]:
+                return False
+        return True
