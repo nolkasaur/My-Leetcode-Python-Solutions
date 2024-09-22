cd30f40aa0ced8c5c5e7a8fb53b729322567f000
Bruno Firmino <brunoftlk@gmail.com>
2024-09-22 12:24:42 +0100

    Create 0104_recursive.py

---
+++ Problem Listing/0104_recursive.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/maximum-depth-of-binary-tree/
+# 41 ms, 17.70 MB
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return max(1 + self.maxDepth(root.left), 1 + self.maxDepth(root.right))
